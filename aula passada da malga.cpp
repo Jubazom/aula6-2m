b297b272288cb864a867516d662e935fcc5fe05d
jubazom <julianobuzatto02@gmail.com>
2024-09-16 09:51:14 -0300

    versão inicial do projeto

---
+++ aula passada da malga.cpp
@@ -0,0 +1,15 @@
+#include <locale.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+int main ()
+
+{
+	int cod;
+	system("color 0A");
+	setlocale(LC_ALL, "PORTUGUESE");
+	
+	printf("\n B E M  V I N D O  A  C A L C U L A D O R A  M A T E M Á T I C A \n");
+	
+}
